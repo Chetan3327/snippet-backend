dcbcc576d31a0e0be9968531e57aa6e416ed6d1b
Chetan3327 <chauhanchetan12789@gmail.com>
2023-12-13 14:30:19 +0530

    put and delete route for sections

--- routes/section.js
+++ routes/section.js
@@ -41,5 +41,38 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.put('/:sectionId', async (req, res) => {
+    try {
+        const sectionId = req.params.sectionId
+        const section = await SectionModel.findById(sectionId)
+        if(!section){
+            return res.status(404).json({error: 'section not found'})
+        }
+        const updatedSection = await SectionModel.findByIdAndUpdate(sectionId, {snippets: req.body.snippets})
+        if(!updatedSection){
+            return res.status(500).json({ error: 'section not Updated' });
+        }
+        res.status(200).json({ message: 'Section Updated successfully' })
+    } catch (error) {
+        res.status(500).json({error: "Internal server error"})
+    }
+})
+
+router.delete('/:sectionId', async (req, res) => {
+    try {
+        const section = await SectionModel.findById(req.params.sectionId)        
+        if(!section){
+            return res.status(404).json({ error: 'section not found' });
+        }
+        const deletedSection = await SectionModel.findByIdAndDelete(req.params.sectionId)
+        if (!deletedSection) {
+            return res.status(500).json({ error: 'snippet not Deleted' });
+        }
+        res.status(200).json({ message: 'Snippet Deleted successfully' })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error: "Internal server error"})
+    }
+})
 
 export default router
